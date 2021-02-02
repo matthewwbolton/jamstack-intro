e801aa5fd61ab5301096be8b1931e8f693741b4d
Matthew Bolton <matthewwbolton@gmail.com>
2021-02-02 11:31:10 -0500

    created form.js and imported form component into index.js - created styles module for form component

---
+++ sections/contact/src/components/form.js
@@ -0,0 +1,28 @@
+import React from "react";
+import styles from "./form.module.css";
+
+const Form = () => {
+  return (
+    <form className={styles.form}>
+      <label className={styles.label}>
+        Name
+        <input className={styles.input} type="text" name="name" />
+      </label>
+      <label className={styles.label}>
+        Email
+        <input className={styles.input} type="email" name="email" />
+      </label>
+      <label className={styles.label}>
+        Subject
+        <input className={styles.input} type="text" name="subject" />
+      </label>
+      <label className={styles.label}>
+        Body
+        <textarea className={styles.input} name="body" />
+      </label>
+      <button className={styles.button}>Send</button>
+    </form>
+  );
+};
+
+export default Form;
